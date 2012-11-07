7bf10e99ee375c7975f245617ced33db628c1d00
Matt Emborsky <memborsky@gmail.com>
2012-11-07 17:53:50 -0500

    zsh: Some aliases to make my life a little easier.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+# My personal aliases for making my life easier
+# Listing files
+alias ls='/bin/ls -A --color'  # Default ls to show everything but . and .. and colorize it.
+alias ll='ls -lh'         # List it long and make the output in human readable formats.
+
+# Removing files
+alias rm='/bin/rm -iv'         # Make sure we prompt for removal and show what we are removing when it happens."
