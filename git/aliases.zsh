fcb66ab6ee634e0acbe1f5158561ab57f04f16d1
Matt Emborsky <memborsky@gmail.com>
2012-12-12 15:19:59 -0500

    Adjust glog a little to my tastes.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,7 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(bold blue) %an%Creset:%C(yellow)%d%Creset %s%Cgreen (%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gph='git push heroku HEAD'
 alias gd='git diff'
