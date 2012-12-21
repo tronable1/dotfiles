c914fd2aa1c1ebffbb7f33e02e5a2fa857dfafd5
Matt Emborsky <memborsky@gmail.com>
2012-12-20 21:37:30 -1000

    Add support for linux based ls aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,17 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
+if [ $(uname -s) = "Darwin" ]
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la="gls -A --color"
+  if $(gls &>/dev/null)
+  then
+    alias ls="gls -FA --color"
+    alias l="gls -lh --color"
+    alias ll="gls -lh --color"
+  fi
+elif [ $(uname -s) = "Linux" ]
+then
+  alias ls="ls -A --color"
+  alias ll="ls -lh"
+  alias l="ll"
 fi
