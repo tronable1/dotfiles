8aa8b38f9e8c1630152ab3ffd0c89f1e3ad761b6
Matt Emborsky <memborsky@gmail.com>
2012-12-20 20:53:42 -1000

    Add /home/matt/.rbenv/bin to PATH so that we can use rbenv without ruby installed globally.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/python:$PATH"
+export PATH="./bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/python:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
