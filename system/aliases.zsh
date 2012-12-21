ffd5029960f3521f32cb1490e3b63e7a30711a74
Matt Emborsky <memborsky@gmail.com>
2012-12-21 04:00:46 -0500

    This is apparently the only method that will give us both linux and mac ls/ll/l support. The previous method was failing to load gls when opening a new terminal tab with Mac OS X 10.7 Terminal.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,17 +1,13 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if [ $(uname -s) = "Darwin" ]
-then
-  if $(gls &>/dev/null)
-  then
-    alias ls="gls -FA --color"
-    alias l="gls -lh --color"
-    alias ll="gls -lh --color"
-  fi
-elif [ $(uname -s) = "Linux" ]
-then
+if [ $(uname -s) = "Linux" ]; then
   alias ls="ls -A --color"
-  alias ll="ls -lh"
-  alias l="ll"
+elif [ $(uname -s) = "Darwin" ]; then
+  if [ -x "/usr/local/bin/gls" ]; then
+    alias ls="gls -FA --color"
+  fi
 fi
+
+alias ll="ls -lh"
+alias l="ll"
