5ad6378448366076b880a7872ea0ab60572096d0
Matt Emborsky <memborsky@gmail.com>
2013-01-12 00:14:30 -0500

    Do big boy homebrew install if we pass in a file to output a log too. This assumes nothing by default and fails without ruby or curl (or wget as a backup to curl) installed.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,10 +8,94 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  x You should probably install Homebrew first:"
-  echo "    https://github.com/mxcl/homebrew/wiki/installation"
-  exit
+  if [ -z $1 ]
+  then
+    echo "  x You should probably install Homebrew first:"
+    echo "    https://github.com/mxcl/homebrew/wiki/installation"
+    exit
+  else
+    info 'Checking for brew'
+    if test ! $(which brew)
+    then
+      user 'brew installation'
+      while true; do
+        printf " - Would you like to install homebrew [y]es/[n]o? "
+        read -n 1 action
+
+        case "$action" in
+          y )
+            if test ! $(which ruby)
+            then
+              echo "You need ruby installed to install brew.\n" >> $1
+              echo "See http://ruby-lang.org for more information.\n" >> $1
+              fail 'Ruby not installed'
+            fi
+
+            if test ! $(which curl)
+            then
+              if test ! $(which wget)
+              then
+                echo "You need either curl or wget installed to install brew.\n" >> $1
+                echo "Please install one or the other before continuing.\n" >> $1
+                fail 'No curl or wget installed'
+              else
+                HOMEBREW="$(wget -qO - raw.github.com/mxcl/homebrew/go)"
+              fi
+            else
+              HOMEBREW="$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+            fi
+
+            ruby -e $HOMEBREW
+
+            BREW_INSTALLED=true
+            break
+            ;;
+
+          n )
+            echo "Homebrew NOT installed." >> $1
+            success 'Homebrew NOT installed.'
+            BREW_INSTALLED=false
+            break
+            ;;
+
+          * )
+            echo "Please try again (y/n)"
+            continue
+            ;;
+        esac
+      done
+    fi
+  fi
 fi
 
-# Install homebrew packages
-brew install grc coreutils spark
+if [ -z $1 ]
+then
+  # Install homebrew packages
+  brew install grc coreutils spark
+else
+  brew_installed () {
+    brew list | grep -i $1 > /dev/null
+    [ $? -eq 0 ]
+  }
+
+  brew_install () {
+    if brew_installed $1
+    then
+      success $1
+      return
+    fi
+
+    info "installing $1"
+    if $2 >> $output
+    then
+      brew install $1 >> $output
+      success $1
+    else
+      fail $1
+    fi
+  } 
+
+  brew_install grc
+  brew_install coreutils
+  brew_install spark
+fi
