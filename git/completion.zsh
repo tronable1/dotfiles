2d16f288c7bd36eace8e995cd44fbd1fe68e992e
Matt Emborsky <memborsky@gmail.com>
2013-03-11 11:35:00 -0400

    Fix git completion issues.

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,9 +1,13 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+completion_git=/usr/local/share/zsh/site-functions/_git
+completion_hub=/usr/local/share/zsh/site-functions/_hub
 
-if test -f $completion
+if [ -f $completion_hub ]
 then
-  source $completion
+  export FPATH="$completion_hub:$FPATH"
+elif [ -f $completion_git ]
+then
+  export FPATH="$completion_git:$FPATH"
 fi
